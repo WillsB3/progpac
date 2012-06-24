793537689fdeecfd68a6298b7fd67d7ff9039336
xando <sebastian.pawlus@gmail.com>
2012-06-24 17:32:22 +0100

    Static directory moved.

--- progpac/static/game/init.js
+++
@@ -1,9 +0,0 @@
-Function.prototype.method = function (name, func) {
-  this.prototype[name] = func;
-  return this;
-}
-
-Object.method('debug', function (toprint) {
-  if (console !== undefined && console.log !== undefined)
-    console.log(toprint);
-});
